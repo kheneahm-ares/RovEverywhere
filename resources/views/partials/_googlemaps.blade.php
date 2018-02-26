3442cfe3cc278c4b727139167bde4e4434f5e059
amazing-username <kdeng02@aurora.edu>
2018-02-26 09:43:54 -0600

    Initial commit for the mobile app branch

--- resources/views/partials/_googlemaps.blade.php
+++
@@ -1,8 +0,0 @@
-<style>
-  #map-canvas{
-      width: 500px;
-      height: 500px;
-  }
-</style>
-
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvuPRT9TEk3uWyoC4-nIw7tOI-0wuhbvA&libraries=places"></script>
