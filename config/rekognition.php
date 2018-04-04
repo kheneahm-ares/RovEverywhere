74a3b4427fd716435ff074f621c8281651cda7b0
mr-robot9 <kheneahm9@yahoo.com>
2018-04-03 22:08:52 -0500

    ignore

--- config/rekognition.php
+++ config/rekognition.php
@@ -2,8 +2,8 @@
 
 return [
     'credentials' => [
-        'key'    => '',
-        'secret' => '',
+        'key'    => 'AKIAIIZBPZP5BHWFS2AQ',
+        'secret' => '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',
     ],
     'region' => 'us-east-1',
     'version' => 'latest'
