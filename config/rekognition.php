4e90042dce90573c71786c23854d305e7399a883
mr-robot9 <kheneahm9@yahoo.com>
2018-04-06 14:26:24 -0500

    configs

--- config/rekognition.php
+++ config/rekognition.php
@@ -2,8 +2,8 @@
 
 return [
     'credentials' => [
-        'key'    => 'AKIAJQVLYV2PIAYK7M4Q',
-        'secret' => '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',
+        'key'    => '',
+        'secret' => '',
     ],
     'region' => 'us-east-1',
     'version' => 'latest'
