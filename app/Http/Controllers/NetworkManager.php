4b40473d12bda908a8d782f0d279c55471bcd8f3
amazing-username <kdeng02@aurora.edu>
2018-04-19 21:24:07 -0500

    Continuing work on the NetworkManager

--- app/Http/Controllers/NetworkManager.php
+++ app/Http/Controllers/NetworkManager.php
@@ -13,6 +13,7 @@ class NetworkManager extends Controller
 
 		return view('network.index');
 	}
+	//Adding controlllers
 	public function add(){
 		return view('network.add');
 	}
@@ -39,10 +40,28 @@ class NetworkManager extends Controller
 
 		return view('network.index');
 	}
-	public function
+	public function newmschapv2(){
+		return view('network.newmschapv2form');
+	}
+	public function mschapv2(Request $request){
+
+		$ssid = $request->input('ssid');
+		$eap = $request->input('eap');
+		$identity = $request->input('identity');
+		$password = $request->input('password');
+		$phase1 = $request->input('phase1');
+		$phase2 = $request->input('phase2');
+
+
+		return view('network.index');
+	}
+
+	//Editing controllers
 	public function edit(){
 		return view('network.edit');
 	}
+
+	//Destroying controllers
 	public function destroy(){
 		return view('network.destroy');
 	}
---
+++ resources/views/network/newmschapv2form.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+</style>
+  <?php
+    header_remove();
+?>
+
+<div class="col-md-12">
+{{ Form::open(array('url' => '/network/add/mschapv2')) }}
+
+	{{ Form::label('ssidLbl', "ssid") }}
+	{{ Form::text('ssid') }}
+	<br>
+	{{ Form::label('eapLbl', 'eap') }}
+	{{ Form::text('eap') }}
+	<br>
+	{{ Form::label('identityLbl', 'identity') }}
+	{{ Form::text('identity') }}
+	<br>
+	{{ Form::label('passwordLbl', 'password') }}
+	{{ Form::password('password') }}
+	<br>
+	{{ Form::label('phase1Lbl', 'phase1') }}
+	{{ Form::text('phase1') }}
+	<br>
+	{{ Form::label('phase2Lbl', 'phase2') }}
+	{{ Form::text('phase2') }}
+	<br>
+	{{ Form::submit('Add') }}
+
+{{ Form::close() }} 
+</div>
+@endsection
